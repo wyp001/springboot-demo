a5ad2bd5509d5f48694285fcb1b38f8577630490
wyupeng <wyupeng@linewell.com>
2019-06-21 16:11:31 +0800

    使用@bean（name=“xxx”）

--- src/main/java/com/example/test/BeanDemo.java
+++ src/main/java/com/example/test/BeanDemo.java
@@ -3,20 +3,17 @@ package com.example.test;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
 
 //@Service
 @Configuration
 public class BeanDemo {
 
-    @Bean
+    @Bean(name = "persion")
     @Scope("prototype")
     public BeanDemo getBean(){
         BeanDemo beanDemo = new BeanDemo();
         System.out.println("调用方法： "+ beanDemo);
         return beanDemo;
     }
-
-
 
 }
